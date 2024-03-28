ebf7c726535df15c57b2c8120cbabbb6556f0365
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-03-28 13:09:58 +0530

    formatted string

---
+++ Python/formatted.py
@@ -0,0 +1,11 @@
+first_name = "Gyanendra"
+
+last_name = "Prakash"
+
+print(first_name)
+print(last_name)
+
+print(first_name+" "+last_name)  # concat two string.
+
+full_name = f"{first_name} {last_name}"  # Add formatted string
+print(full_name)
