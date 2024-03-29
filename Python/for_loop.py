7d8a5ff47911938892b0859d625a7fe40b49bce4
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-03-29 10:36:52 +0530

    for loop

---
+++ Python/for_loop.py
@@ -0,0 +1,16 @@
+
+# loop used for iterative execution
+
+for number in range(4, 6):
+    print("Hello...")
+
+
+# while loop
+
+num = 2
+
+while num <= 10:
+    print(num)
+    num = num+1
+
+print("While loop End")
