28c70979202afcb6bad3acd9b426cf838923b88e
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-03-29 10:37:04 +0530

    logical operator

---
+++ Python/logical_operatot.py
@@ -0,0 +1,19 @@
+# Logical OR
+# Logical NOT
+# Logical AND
+
+ssc = 70
+hsc = 80
+
+if ssc >= 60 and hsc >= 65:
+    print("You Are Eligible")
+else:
+    print("Not Eligible")
+
+
+# Ternery Operator
+
+    age = 22
+
+
+print("can Vote") if age >= 21 else print("Can not Vote")
