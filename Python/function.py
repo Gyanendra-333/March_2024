626b9b2a8d0bba28a072c0fc0694519a0f5244d2
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-03-30 11:15:50 +0530

    function

---
+++ Python/function.py
@@ -0,0 +1,16 @@
+
+def sayHello():
+    print("Hello World")
+
+
+sayHello()
+
+# print table of two
+
+
+def tableOfTwo():
+    for number in range(1, 11):
+        print(f"2 x {number} = {number*2}")
+
+
+tableOfTwo()
