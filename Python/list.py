4ae106a47bcbc54c9f46222b0fc24acf513b5138
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-03-31 18:37:57 +0530

    list

---
+++ Python/list.py
@@ -0,0 +1,16 @@
+
+data = "Gyanendra"
+
+names = ["Ram", "Shyam", "Aman", "Daman", 4, 57, 3545, 23]
+
+print(type(names))
+print(names)
+
+# Access List
+
+print(names[3])
+
+# List Slicing
+
+new_slic = names[2:5]
+print(new_slic)
